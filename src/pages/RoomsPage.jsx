45b19c84996b14237b53500c5ccec4f382c65667
Yogesh Malkani <yogesh.malkani@pillup.com>
2024-03-16 13:28:35 +0530

    added rooms page

--- src/pages/RoomsPage.jsx
+++ src/pages/RoomsPage.jsx
@@ -12,11 +12,11 @@ export default function RoomsPage() {
     return (
         <div className='py-20 flex flex-col justify-center items-center gap-10 font-Lora '>
             <p className='text-3xl max-md:text-xl'>Rooms at the {HOTEL_NAME}</p>
-            <div className='flex flex-row flex-wrap gap-5 w-[90%]'>
+            <div className='flex flex-row flex-wrap justify-center items-center  gap-5 w-[90%]'>
                 {
                     Object.keys(HOTEL_ROOM_TYPES_AND_INFO).map((type, ind) => {
                         return (
-                            <div key={ind} className=''> <ProductCard clickHandler={() => clickHandler(type)} heading={type} description={HOTEL_ROOM_TYPES_AND_INFO[type].description} imageLocation={"images/room-types/" + HOTEL_ROOM_TYPES_AND_INFO[type].photos[0] + ".jpg"} />
+                            <div key={ind} className='w-[25rem]'> <ProductCard clickHandler={() => clickHandler(type)} heading={type} description={HOTEL_ROOM_TYPES_AND_INFO[type].description} imageLocation={"images/room-types/" + HOTEL_ROOM_TYPES_AND_INFO[type].photos[0] + ".jpg"} />
                             </div>)
                     })
                 }
