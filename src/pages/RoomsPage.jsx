833be44ca6f1d2e3a6e9f8d795a04c49e7946fc9
Yogesh Malkani <yogesh.malkani@pillup.com>
2024-03-16 13:14:29 +0530

    added rooms page

--- src/pages/RoomsPage.jsx
+++ src/pages/RoomsPage.jsx
@@ -12,7 +12,7 @@ export default function RoomsPage() {
     return (
         <div className='py-20 flex flex-col justify-center items-center gap-10 font-Lora '>
             <p className='text-3xl max-md:text-xl'>Rooms at the {HOTEL_NAME}</p>
-            <div className='flex flex-row gap-5 w-[90%]'>
+            <div className='flex flex-row flex-wrap gap-5 w-[90%]'>
                 {
                     Object.keys(HOTEL_ROOM_TYPES_AND_INFO).map((type, ind) => {
                         return (
